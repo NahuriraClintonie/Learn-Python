f41f45fe3a53fe9e47ed0a551c32b07104c135e0
NahuriraClintonie <clintonnahurira24@gmail.com>
2024-11-06 14:02:44 +0300

    changing a string to upper case, lower case or printing it's title

---
+++ Strings/StringMethods.py
@@ -0,0 +1,11 @@
+# Creating strings
+greeting = "Hello, World!"
+name = 'Alice'
+
+
+# Changing case
+print(greeting.upper())  # "HELLO, WORLD!"
+print(greeting.lower())  # "hello, world!"
+print(greeting.title())  # "Hello, World!"
+
+
