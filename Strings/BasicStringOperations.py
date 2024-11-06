60af738834243ed5b4009ed6d12984a5229666ae
NahuriraClintonie <clintonnahurira24@gmail.com>
2024-11-06 13:55:12 +0300

    how to initialize strings, accessing characters at given indices within a given string, slicing and reversing strings,concatenating strings and then printing them

---
+++ Strings/BasicStringOperations.py
@@ -0,0 +1,16 @@
+# Creating strings
+greeting = "Hello, World!"
+name = 'Alice'
+
+# Accessing characters
+first_char = greeting[0]      # 'H'
+last_char = greeting[-1]      # '!'
+
+# Slicing
+sub_string = greeting[0:5]    # 'Hello'
+reversed_greeting = greeting[::-1]  # '!dlroW ,olleH'
+
+# Concatenation
+full_greeting = greeting + " " + name  # "Hello, World! Alice"
+
+print(full_greeting)
