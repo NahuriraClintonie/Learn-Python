54b3388123d578942982c88b8c51e50035df66e3
NahuriraClintonie <clintonnahurira24@gmail.com>
2024-11-06 14:03:47 +0300

    removing white space from a string

--- Strings/StringMethods.py
+++ Strings/StringMethods.py
@@ -8,4 +8,6 @@ print(greeting.upper())  # "HELLO, WORLD!"
 print(greeting.lower())  # "hello, world!"
 print(greeting.title())  # "Hello, World!"
 
-
+# Removing whitespace
+text = "   Hello, Python!   "
+print(text.strip())  # "Hello, Python!"
